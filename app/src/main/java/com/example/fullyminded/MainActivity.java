fd686bcdc4e4887722fa7c50879beaea6df9de7d
Sierra Riley <w0791053@myscc.ca>
2023-11-13 11:00:59 -0500

    Preparing to merge

--- app/src/main/java/com/example/fullyminded/MainActivity.java
+++ app/src/main/java/com/example/fullyminded/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.fullyminded;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -61,5 +63,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_settings:
+        }
     }
 }
