1b2c65e494b85391cd5cb8f55575b27b32a1ca88
Sierra Riley <w0791053@myscc.ca>
2023-11-20 10:06:52 -0500

    Created Buttons

---
+++ app/src/main/java/com/example/fullyminded/FormFragment.java
@@ -0,0 +1,93 @@
+package com.example.fullyminded;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FormFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FormFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FormFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CallFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FormFragment newInstance(String param1, String param2) {
+        FormFragment fragment = new FormFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_call, container, false);
+
+       Button call = view.findViewById(R.id.signUpCallButton);
+       call.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent i = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:5191857439"));
+//               if(i.resolveActivity(getPackageManager()) != null){
+                   startActivity(i);
+//               }
+
+           }
+       });
+
+
+        Button formButton = view.findViewById(R.id.formButton);
+        formButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_sign_to_callFragment);
+            }
+        });
+
+
+        return view;
+    }
+}
